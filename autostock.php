da9092cd247d943945139f24f6712d4cd1099a4e
Kevin Phillips <kevin@bestnet.co.nz>
2015-01-01 13:09:23 +1300

    Added new plugin main file with GPL info

---
+++ autostock.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name: Autostock
+ * Plugin URI: http://kevinphillips.co.nz
+ * Description: Car dealer stock management plugin for WordPress.
+ * Version: 1.0.0
+ * Author: Kevin Phillips
+ * Author URI: http://kevinphillips.co.nz
+ * Text Domain: autostock
+ * Domain Path: locale
+ * license: GPL2
+ */
+
+/*  Copyright 2015  Kevin Phillips  (email : kevin@kevinphillips.co.nz)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
